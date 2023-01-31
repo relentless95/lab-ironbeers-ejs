b48456493cfe4ea900e2148fef47de123d98f5f4
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-01-31 17:54:58 +0100

    iteration2

--- app.js
+++ app.js
@@ -11,12 +11,24 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async(req, res) => {
+  try{let allBeers = await punkAPI.getBeers()
+    console.log('heres the beers', allBeers)
+    res.render('index',{allBeers});}
+  
+  catch(error){
+    console.log('Error geting the beers', error)
+  }
 });
+
+// app.get('/', (req,res)=>{
+
+// })
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
